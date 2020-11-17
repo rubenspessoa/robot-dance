05909c552b470b4571f725d2991ae4aad66e826f
Rubens Pessoa De Barros Filho <3586658+rubenspessoa@users.noreply.github.com>
2020-11-17 14:57:00 +0100

    add LOGGING to .env files

--- robot-dance/api/src/app.module.ts
+++ robot-dance/api/src/app.module.ts
@@ -1,13 +1,13 @@
-import {Module} from '@nestjs/common';
-import {RobotsModule} from "./robots/robots.module";
-import {TypeOrmModule} from "@nestjs/typeorm";
-import {Connection} from "typeorm";
+import {DanceBattle} from './dance-battles/dance-battle.entity';
 import {DanceBattlesModule} from './dance-battles/dance-battles.module';
+import {DanceOff} from './dance-offs/dance-off.entity';
 import {DanceOffsModule} from './dance-offs/dance-offs.module';
-import {Robot} from "./robots/robot.entity";
-import {DanceBattle} from "./dance-battles/dance-battle.entity";
-import {DanceOff} from "./dance-offs/dance-off.entity";
-import {ConfigModule, ConfigService} from "@nestjs/config";
+import {Robot} from './robots/robot.entity';
+import {RobotsModule} from './robots/robots.module';
+import {Module} from '@nestjs/common';
+import {ConfigModule, ConfigService} from '@nestjs/config';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import {Connection} from 'typeorm';
 
 const configService = new ConfigService();
 
@@ -15,15 +15,19 @@ const configService = new ConfigService();
     imports: [
         ConfigModule.forRoot({isGlobal: true}),
         TypeOrmModule.forRoot({
-            "type": "sqlite",
-            "database": "database.db",
-            "logging": true,
-            "synchronize": configService.get<boolean>('SHOULD_SYNCHRONISE'),
-            "entities": [Robot, DanceBattle, DanceOff]
+            type: 'mysql',
+            host: 'localhost',
+            port: 3306,
+            username: 'root',
+            password: 'robotdance',
+            database: 'robot_dance',
+            logging: configService.get<boolean>('LOGGING'),
+            synchronize: configService.get<boolean>('SYNCHRONIZE'),
+            entities: [Robot, DanceBattle, DanceOff],
         }),
         RobotsModule,
         DanceBattlesModule,
-        DanceOffsModule
+        DanceOffsModule,
     ],
     controllers: [],
     providers: [],
